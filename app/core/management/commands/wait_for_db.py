de3aa17cef0f431ed648a06871f250bdafdca897
bandaural <64231032+bandaural@users.noreply.github.com>
2024-02-18 01:17:16 -0300

    wait for db first part

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,12 @@
+"""
+Django command to wait for the database to be avaliable.
+"""
+
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    """Django command to wait for database."""
+
+    def hadle(self, *args, **options):
+        pass
