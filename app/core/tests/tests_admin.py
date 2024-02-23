ad4a0774a7278b8206dc6d309e5a3c8de9311e9f
bandaural <64231032+bandaural@users.noreply.github.com>
2024-02-23 00:29:34 -0300

    admin page (tests and functionality)

---
+++ app/core/tests/tests_admin.py
@@ -0,0 +1,36 @@
+"""
+Tests for the Django Admin modifications.
+"""
+
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import Client
+
+
+class AdminSiteTests(TestCase):
+    """Tests for Django admin."""
+
+    def setUp(self):
+        """Create a user and client for tests"""
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@admin.com',
+            password='12345678'
+        )
+        self.client.force_login(self.admin_user)
+        self.user =- get_user_model().objects.create_user(
+            email='test@test.com',
+            password='12345678',
+            name='Username'
+        )
+        self.client.force_login(self.user)
+
+    def test_users_list(self):
+        """Test that users are listed on page. """
+        url = reverse('admin:core_user_changelist') #Documentation https://docs.djangoproject.com/en/3.1/ref/contrib/admin/#reversing-admin-urls
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
+
