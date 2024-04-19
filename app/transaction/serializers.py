d36d70f831bb29a579817317bf0614661c31c3bb
bandaural <64231032+bandaural@users.noreply.github.com>
2024-04-19 00:00:00 -0400

    refactor

--- app/transaction/serializers.py
+++ app/transaction/serializers.py
@@ -20,52 +20,10 @@ class SupplierSerializer(serializers.ModelSerializer):
         model = Supplier
         fields =['id', 'name', 'category']
 
-    def create(self, validated_data):
-        category_data = validated_data.pop('category')
-        print(category_data)
-
-        request = self.context.get('request')
-        auth_user = request.user
-        print(auth_user)
-
-        category, created = Category.objects.get_or_create(
-                                            user = auth_user,
-                                            **category_data)
-        print(category)
-
-        supplier_data = validated_data.pop('supplier', [])
-        print(supplier_data)
-        supplier = Supplier.objects.get_or_create(
-                                            user = auth_user,
-                                            category=category,
-                                            **supplier_data)
-        print(supplier)
-        return supplier
-
-    def update(self, instance, validated_data):
-        category_data = validated_data.pop('category')
-        print(validated_data)
-
-        request = self.context.get('request')
-        auth_user = request.user
-        print(auth_user)
-
-        category, created = Category.objects.get_or_create(
-                                            user = auth_user,
-                                            **category_data
-        )
-        print(category)
-
-
-        name = instance.name
-        instance.name = validated_data.get('name', instance.name)
-        instance.save()
-        return instance
-
 class TransactionSerializer(serializers.ModelSerializer):
     """Serializer for transactions. """
-    category = CategorySerializer()
-    supplier = SupplierSerializer()
+    category = CategorySerializer(required=False)
+    supplier = SupplierSerializer(required=False)
     class Meta:
         model = Transaction
         fields = ['id',
@@ -81,11 +39,21 @@ class TransactionSerializer(serializers.ModelSerializer):
                  'supplier']
         read_only_fields = ['id']
 
+        def create(self, validated_data):
+            category = validated_data.pop('category')
+            supplier = validated_data.pop('supplier')
 
-    def create(self, validated_data):
+            auth_user = self.context['request'].user
+            category, created = Category.objects.get_or_create(user=auth_user, **category)
+            supplier, created = Supplier.objects.get_or_create(user=auth_user, category=category, **supplier)
+
+            transaction = Transaction.objects.create(category=category, supplier=supplier, **validated_data)
+            print(transaction)
+            return transaction
+
+
+        """def create(self, validated_data):
         category_data = validated_data.pop('category')
-        print(category_data)
-
         request = self.context.get('request')
         auth_user = request.user
         print(auth_user)
@@ -108,10 +76,9 @@ class TransactionSerializer(serializers.ModelSerializer):
                                     supplier=supplier,
                                     **validated_data)
         print(transaction)
-        return transaction
 
-    def update(self, validated_data):
-        pass
+        return transaction"""
+
 
 
 
